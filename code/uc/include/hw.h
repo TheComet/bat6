b4e21e506a1d7ee3e3fcdca8fefc7ff9513a97a4
Alex Murray <alex.murray@gmx.ch>
2015-10-18 15:15:29 +0200

    Initial commit of uC project + code

---
+++ code/uc/include/hw.h
@@ -0,0 +1,22 @@
+/* 
+ * @file hw.h
+ * @author Alex Murray
+ *
+ * Created on 18 October 2015, 14:50
+ */
+
+#ifndef HW_H
+#define	HW_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* HW_H */
+
---
+++ code/uc/src/main.c
@@ -0,0 +1,19 @@
+/*!
+ * @file main.c
+ * @author Alex Murray
+ * 
+ * Created on 18 October 2015, 14:34
+ */
+
+#include <stdlib.h>
+#include "init.h"
+
+/* -------------------------------------------------------------------------- */
+void main(void)
+{
+    init_device();
+    
+    while(1)
+    {
+    }
+}
