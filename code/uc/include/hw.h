1b81a9f1f88888ef1d3c6778ce36517d4664c99f
Alex Murray <alex.murray@gmx.ch>
2015-11-11 23:39:06 +0100

    fixes #26 - Clock module configured for 60 MIPS

--- code/uc/include/hw.h
+++ code/uc/include/hw.h
@@ -1,4 +1,4 @@
-/* 
+/*! 
  * @file hw.h
  * @author Alex Murray
  *
@@ -11,6 +11,73 @@
 #ifdef	__cplusplus
 extern "C" {
 #endif
+
+/* Other files should include hw.h instead of the following header */
+#include "p33EP16GS506.h"
+
+/*
+ * _____________________________________________________________________________
+ *                              Port Mappings
+ * -------+--------------+-----------+------------------------------------------
+ *  Port  |  HW name     | Function  | Description
+ * _______|______________|___________|__________________________________________
+ *  RA.0  | BUCK_IMEAS_N | PGA1P1    | Negative differential input from current
+ *        |              |           | sense resistor on output.
+ *        |              |           | Biased to 3.3/2V.
+ * -------+--------------+-----------+------------------------------------------
+ *  RA.1  | BUCK_UMEAS   | AN1       | Feedback voltage from output voltage
+ *        |              |           | divider. Scaled to 0..1.5V. Use VREF from
+ *        |              |           | AN8 to derive an accurate value of the
+ *        |              |           | output voltage, as the 3.3 rail is
+ *        |              |           | unstable.
+ * -------+--------------+-----------+------------------------------------------
+ *  RA.2  | BUCK_EN      | GPIO/O    | When high, the buck is enabled. If the
+ *        |              |           | 36V rail sinks below 25V, the buck is
+ *        |              |           | automatically disabled and an interrupt
+ *        |              |           | is generated over BUCK_UVLO.
+ * -------+--------------+-----------+------------------------------------------
+ *  RB.2  | BUCK_IMEAS_P | PGA1N2    | Positive differential input from current
+ *        |              |           | sense resistor on output. Biased to
+ *        |              |           | 3.3V/2V. Will change linearly in function
+ *        |              |           | with output current where 3A = 15mV.
+ * -------+--------------+-----------+------------------------------------------
+ *  RB.3  | BUCK_USET    | DACOUT1   | Sets the regulated output voltage.
+ *        |              |           | Use EXTREF1 for higher accuracy.
+ * -------+--------------+-----------+------------------------------------------
+ *  RB.8  | LCD_SDA      | SDA2      | I2C SDA line for LCD
+ * -------+--------------+-----------+------------------------------------------
+ *  RB.11 | LCD_RESET    | GPIO/O    | Active low reset
+ * -------+--------------+-----------+------------------------------------------
+ *  RB.15 | LCD_SCL      | SCL2      | I2C SCL line for LCD
+ * -------+--------------+-----------+------------------------------------------
+ *  RC.1  | VREF         | AN8       | 1.5V reference voltage can be sampled
+ * -------+--------------+-----------+------------------------------------------
+ *  RC.2  | VREF         | EXTREF1   | 1.5V reference voltage input for DACs
+ * -------+--------------+-----------+------------------------------------------
+ *  RC.3  | LED0         | PWM5L     | PWM controlled LED
+ * -------+--------------+-----------+------------------------------------------
+ *  RC.4  | KNOB_A       | INT       | Interrupt for rotary encoder, line A
+ * -------+--------------+-----------+------------------------------------------
+ *  RC.5  | KNOB_B       | INT       | Interrupt for rotary encoder, line B
+ * -------+--------------+-----------+------------------------------------------
+ *  RC.6  | KNOB_BTN     | INT       | Interrupt for button press/release
+ * -------+--------------+-----------+------------------------------------------
+ *  RC.9  | BUCK_UVLO    | INT       | Goes high during power failure.
+ * -------+--------------+-----------+------------------------------------------
+ *  RC.11 | RX           | RP59/UART | UART receive
+ * -------+--------------+-----------+------------------------------------------
+ *  RC.12 | TX           | RP60/UART | UART transmit
+ * -------+--------------+-----------+------------------------------------------
+ *  RD.1  | LED3         | PWM4H     | PWM controlled LED
+ * -------+--------------+-----------+------------------------------------------
+ *  RD.3  | LED2         | PWM4L     | PWM controlled LED
+ * -------+--------------+-----------+------------------------------------------
+ *  RD.6  | LED1         | PWM5H     | PWM controlled LED
+ * -------+--------------+-----------+------------------------------------------
+ *  RD.13 | BUCK_ISET    | DACOUT2   | Sets the maximum regulated current.
+ *        |              |           | Use EXTREF1 for higher accuracy.
+ * -------+--------------+-----------+------------------------------------------
+ */
 
 
 
--- code/uc/src/init.c
+++ code/uc/src/init.c
@@ -6,22 +6,56 @@
  */
 
 #include "init.h"
+#include "hw.h"
 
 /* -------------------------------------------------------------------------- */
-static void wdt(void)
-{
-    
-}
+/* Disable watchdog timer */
+_FWDT(WDTEN_OFF);
 
 /* -------------------------------------------------------------------------- */
-static void sysclk(void)
+/* 
+ * Oscillator setup - We have to boot using a clock without PLL. We switch
+ * to PLL during initialisation.
+ */
+
+_FOSCSEL(
+    FNOSC_FRC & /* Use Internal Fast RC oscillator -- approx. 7.37 MHz */
+    IESO_OFF);  /* Start up with user selected oscillator source*/
+
+_FOSC(
+    FCKSM_CSECMD &  /* enable clock switching for the switch to PLL to work */
+    OSCIOFNC_ON  &  /* OSC2 is a general purpose IO pin */
+    IOL1WAY_ON &    /* allow only one reconfiguration of clock */
+    POSCMD_NONE);   /* primary oscillator is disabled (we're using internal) */
+
+/* -------------------------------------------------------------------------- */
+static void sysclk60mips(void)
 {
+    /* 
+     * Configure PLL - Target execution speed is Fcy = 60 MIPS
+     *   Fpllo = 2*Fcy = 120 MHz (15 MHz < Fpllo < 120 MHz)
+     *   Fvco = 2*Fpllo = 240 MHz (120 MHz < Fvco < 340 MHz)
+     *   Fplli = 7.37 MHz / 2 = 3.685 MHz
+     *      --> M = 65
+     */
+    PLLFBD = 63;            /* Feedback divider M = 65 */
+    CLKDIVbits.PLLPOST = 0; /* N2 = 2 (can't go lower) */
+    CLKDIVbits.PLLPRE = 0;  /* N1 = 2 (can't go lower)* /
     
+    /* initiate clock switch to 0x01 = FRC oscillator with PLL */
+    __builtin_write_OSCCONH(0x01); /* New oscillator selection bits */
+    __builtin_write_OSCCONL(OSCCON | 0x01); /* requests oscillator switch to
+                                              * the selection specified above */
+    
+    /* wait for clock switch to occur */
+    while(OSCCONbits.COSC != 0x01);
+    
+    /* wait for PLL to lock */
+    while(OSCCONbits.LOCK != 1);
 }
 
 /* -------------------------------------------------------------------------- */
 void init_device(void)
 {
-    wdt();
-    sysclk();
+    sysclk60mips();
 }
