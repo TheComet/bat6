889f486b0acfaec77bf7565127c2589e7010f088
Alex Murray <alex.murray@gmx.ch>
2015-11-15 17:01:55 +0100

    Easier to clean generated files

---
+++ code/uc/clean.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+rm -rf PV-Simulator.X/build
+rm -rf PV-Simulator.X/dist
+rm -rf PV-Simulator.X/debug
+rm -rf PV-Simulator.X/nbproject/private
+rm PV-Simulator.X/nbproject/Makefile-*
+rm PV-Simulator.X/nbproject/Package-*
+
