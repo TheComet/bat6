29b86e4b95b853a828da52778767555a9fbf463b
Alex Murray <alex.murray@gmx.ch>
2015-11-15 17:01:41 +0100

    Merging linked_list with events - having a generic linked list is overkill

--- code/uc/src/event.c
+++ code/uc/src/event.c
@@ -3,26 +3,65 @@
  * @author Alex Murray
  * 
  * Created on 14 November 2015, 21:50
+ * 
+ * Here, a simple event queue is implemented using a static event table and
+ * a dynamic linked list for storing event listeners.
+ * 
+ * Each event has a globally unique "event ID", which is defined in event.h in
+ * the enum "event_id_e". A static table "event_table" is created, mapping each
+ * event ID to a list object.
+ * 
+ * During run-time, listeners can register themselves to any of the list
+ * objects by providing a callback function and the event ID they wish to
+ * register to. A listener_t object is allocated and linked into the list, thus
+ * storing the callback function pointer.
+ * 
+ * The event queue is implemented using a ring buffer. Each entry in the ring
+ * buffer stores the event ID that was posted and optional arguments that were
+ * passed at the time of posting.
+ * 
+ * When the time comes to process all events, we iterate over the ring buffer
+ * and extract the event ID + arguments. We use the event ID to look up the
+ * associated list object in the static event table. Said list object contains a
+ * linked list of all listeners interested in postings of the event. We proceed
+ * to iterate over the listeners and call the associated callback function.
  */
 
 #include "event.h"
 #include "drv/hw.h"
-#include "util/linked_list.h"
 #include <stdlib.h>
 
+/* 
+ * An object of this type is dynamically allocated when a listener registers.
+ */
 struct listener_t
 {
     struct listener_t*          next; /* intrusive linked list */
     event_listener_func         callback;
 };
-static struct linked_list_t* event_table[EVENT_COUNT] = {0};
 
+/* 
+ * For every event ID that exists, a list object is created.
+ */
+struct listener_list_t
+{
+    struct listener_t* head;
+    struct listener_t* tail;
+};
+static struct listener_list_t event_table[EVENT_COUNT] = {{0}};
+
+/* 
+ * Ring buffer storage
+ */
 struct ring_buffer_data_t
 {
     event_id_e                  event_id;
     void*                       args;
 };
 
+/* 
+ * The actual ring buffer
+ */
 #define RING_BUFFER_SIZE 32
 static struct
 {
@@ -32,15 +71,28 @@ static struct
 } ring_buffer = {0};
 
 /* -------------------------------------------------------------------------- */
+static void destroy_listeners(struct listener_list_t* list)
+{
+    struct listener_t* next;
+    struct listener_t* listener = list->head;
+    while(listener)
+    {
+        next = listener->next;
+        free(listener);
+        listener = next;
+    }
+    
+    list->head = NULL;
+    list->tail = NULL;
+}
+
+/* -------------------------------------------------------------------------- */
 void event_init(void)
 {
     unsigned short i = EVENT_COUNT;
     while(i --> 0)
     {
-        if(event_table[i])
-            linked_list_destroy(event_table[i]);
-        
-        event_table[i] = linked_list_create();
+        destroy_listeners(event_table + i);
     }
     
     ring_buffer.read = 0;
@@ -48,14 +100,23 @@ void event_init(void)
 }
 
 /* -------------------------------------------------------------------------- */
-void event_register_listener(event_id_e event, event_listener_func callback)
+void event_register_listener(event_id_e event_id, event_listener_func callback)
 {
+    struct listener_list_t* list;
     struct listener_t* listener = (struct listener_t*)malloc(sizeof *listener);
     if(!listener)
         return;
     
+    listener->next = NULL;
     listener->callback = callback;
-    linked_list_push(event_table[event], (struct linked_list_node_t*)listener);
+    
+    /* link in to list */
+    list = event_table + event_id;
+    if(list->tail) /* set "next" of current tail to new item, if it exists */
+        list->tail->next = listener; 
+    else  /* if list is empty, set head to current item */
+        list->head = listener;
+    list->tail = listener; /* update tail */
 }
 
 /* -------------------------------------------------------------------------- */
@@ -90,26 +151,25 @@ void event_process_all(void)
     unsigned char write, read;
     struct listener_t* listener;
     struct ring_buffer_data_t* data;
+    struct listener_list_t* listener_list;
     
     /* 
      * Copy write position, as it could change during event processing.
      */
     read = ring_buffer.read;
     write = ring_buffer.write;
-  
+
     /* process all events up to the write position we acquired */
     while(read != write)
     {
-        /* get data from ring buffer */
-        data = (ring_buffer.data + ring_buffer.read);
+        /* look up data in ring buffer according to read position */
+        data = (ring_buffer.data + read);
         
-        /* look up linked list of listeners associated with event ID */
-        struct linked_list_t* listeners = event_table[data->event_id];
+        /* look up list of listeners associated with event ID */
+        listener_list = event_table + data->event_id;
         
-        /* iterate list of listeners*/
-        for(listener = (struct listener_t*)listeners->head;
-                listener;
-                listener = listener->next)
+        /* iterate list of listeners */
+        for(listener = listener_list->head; listener; listener = listener->next)
         {
             listener->callback(data->args);
         }
