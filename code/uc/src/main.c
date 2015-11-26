7384045db7b4859caef9eff89d53c6e434b71ade
Reto Freivogel <rfreivogel@windowslive.com>
2015-11-26 10:58:06 +0100

    use mplab fixed point

--- code/uc/src/main.c
+++ code/uc/src/main.c
@@ -5,6 +5,9 @@
  * Created on 18 October 2015, 14:34
  */
 
+#include <libq.h>
+#include <stdio.h>
+#include "usr/pv_model.h"
 #include "drv/hw.h"
 #include "drv/leds.h"
 #include "core/event.h"
@@ -16,11 +19,25 @@ int main(void)
      * Event system must be initialised before hardware, because the hardware
      * drivers need to register listeners.
      */
+    
+    pv_cell_t cell = { _Q16ftoi(21.5), _Q16ftoi(3.5), _Q16ftoi(1.5522),
+        _Q16ftoi(1.0)};
+    
+    _Q16 v_is = _Q16ftoi(15.0);
+    _Q16 i_step = _Q16ftoi(0.1);
+    _Q16 i_is = i_step;
+    _Q16 i_max = _Q16ftoi(4);
+
     event_init();
     hw_init();
     
     while(1)
     {
+        _Q16 v = calc_voltage(&cell, v_is, i_is);
+        printf("%ld, %ld;\n", i_is, v);
+        i_is += i_step;
+        if(i_is > i_max)
+            i_is = i_step;
         event_process_all();
     }
 }
--- code/uc/src/usr/pv_model.c
+++ code/uc/src/usr/pv_model.c
@@ -6,80 +6,42 @@
  */
 
 #include <stdint.h>
+#include <libq.h>
 #include "usr/pv_model.h"
 
 /* -------------------------------------------------------------------------- */
 
-static uint16_t _fp_exp(const uint16_t exponent){
-    /* 
-     * calculates exp using a spline approximation
-     * exponent range: 0 to ln(2) 
-     * result range: 1 to 2
-     */
-    
-    static const uint16_t spline_coefs[8][3] = {
-        {5702,    16374,    32768},    
-        {6217,    17856,    35734},    
-        {6780,    19472,    38968},    
-        {7394,    21234,    42495},    
-        {8063,    23156,    46341},    
-        {8793,    25252,    50535},    
-        {9588,    27537,    55109},    
-        {10458,   30029,    60097}        
-    };
-    
-    const uint16_t spline_index = exponent >> 13;
-    const uint32_t delta = exponent << 3;
-    const uint32_t delta2 = (delta * delta) >> 16;
-    const uint32_t delta3 = (delta * delta2) >> 16;
-    
-    uint32_t sum = spline_coefs[spline_index][2];
-    sum += delta * spline_coefs[spline_index][2] >> 16;
-    sum += delta2 * spline_coefs[spline_index][1] >> 16;
-    sum += delta3 * spline_coefs[spline_index][0] >> 16;
-    
-    return sum >> 1;
+static _Q16 Io(const pv_cell_t* cell, const _Q16 vd){
+    return _Q16exp(_Q16div(vd - cell->voc, cell->vt));
 }
 
-static uint16_t fp_exp(const int16_t exponent){
-    /*
-     * calculates exp using the formula exp(x) = 2^k * exp(x - k * ln(2))
-     * range -16 to 1 format: S4.11;
-     * result range 0 to 2 format: Q1.15
-     */
+static _Q16 Id(const pv_cell_t* cell, const _Q16 vd){ 
+    return _Q16mpy(cell->isc, cell->g - Io(cell, vd));
+}
+
+static _Q16 Idiff(const pv_cell_t* cell, const _Q16 rl, const _Q16 vd){
+    _Q16 id = Id(cell, vd);
     
-    int32_t num = (int32_t)exponent * 47274; // 47274 = 1/ln(2) * 2^15
-    int16_t k = num >> 26; // 26 = 11 + 15; 
-    uint16_t x = (num - (((int32_t)k)<<26)) >> 10; 
+    return id - _Q16div(vd, rl);
+}
+
+static _Q16 deltaIdiff(const pv_cell_t* cell, const _Q16 rl, const _Q16 vd){
+    return _Q16neg(_Q16div(_Q16mpy(cell->isc, Io(cell, vd)), cell->vt))
+            - _Q16reciprocalQ16(rl);
+}
+
+_Q16 calc_voltage(const pv_cell_t* cell, const _Q16 v_is, const _Q16 i_is){
+    const _Q16 rl = _Q16div(v_is, i_is);
     
-    if(k > 0){
-        //Overflow return max
-        return 0xffff;
+    _Q16 v_next = v_is;
+    
+    int i;
+    for(i = 0; i<32; i++){
+        _Q16 I_diff = Idiff(cell,rl,v_next);
+        _Q16 delta_I_diff = deltaIdiff(cell, rl, v_next);
+        _Q16 delta_U = _Q16div(I_diff, delta_I_diff);
+        v_next -= delta_U;
     }
-   
-    return _fp_exp(x) >> (-k);
-}
-
-static uint16_t Io_rel(pv_cell_t* cell, const uint16_t vd){
-    /* calculates exp((vd-voc)/vt) */
-    const int16_t diff = vd - cell->voc; //
-    const int16_t exponent = ((int32_t)diff << 13) / cell->vt;
-    return fp_exp(exponent);
-}
-
-static uint16_t Id(pv_cell_t* cell, const uint16_t vd){
-    const int16_t io = Io_rel(cell, vd);
-    const int16_t irel = (cell->g >> 1) - io;
-    const int16_t id = ((int32_t)cell->isc * irel) >> 16;
-    return id;
-}
-
-uint16_t calc_voltage(pv_cell_t* cell, uint16_t voltage_is, int16_t current_is){
-    /*calculates the new voltage depending on the measured voltage and current
-     *voltage format: Q5.11
-     *current format: Q3.13 */
-    const int16_t Idiff = Id(cell, voltage_is) - current_is;
-    const int16_t Udiff = (int32_t)Idiff * voltage_is / current_is;
     
-    return voltage_is + Udiff;
+    return v_next;
 }
