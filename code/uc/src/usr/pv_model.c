3359101991974b8c0cd8db3621be602af21d3775
Reto Freivogel <rfreivogel@windowslive.com>
2015-11-26 11:37:32 +0100

    use binary approx instead of newton

--- code/uc/src/usr/pv_model.c
+++ code/uc/src/usr/pv_model.c
@@ -19,29 +19,22 @@ static _Q16 Id(const pv_cell_t* cell, const _Q16 vd){
     return _Q16mpy(cell->isc, cell->g - Io(cell, vd));
 }
 
-static _Q16 Idiff(const pv_cell_t* cell, const _Q16 rl, const _Q16 vd){
-    _Q16 id = Id(cell, vd);
-    
-    return id - _Q16div(vd, rl);
-}
-
-static _Q16 deltaIdiff(const pv_cell_t* cell, const _Q16 rl, const _Q16 vd){
-    return _Q16neg(_Q16div(_Q16mpy(cell->isc, Io(cell, vd)), cell->vt))
-            - _Q16reciprocalQ16(rl);
-}
-
 _Q16 calc_voltage(const pv_cell_t* cell, const _Q16 v_is, const _Q16 i_is){
     const _Q16 rl = _Q16div(v_is, i_is);
     
-    _Q16 v_next = v_is;
+    _Q16 v_min = 0;
+    _Q16 v_max = cell->voc;
     
     int i;
     for(i = 0; i<32; i++){
-        _Q16 I_diff = Idiff(cell,rl,v_next);
-        _Q16 delta_I_diff = deltaIdiff(cell, rl, v_next);
-        _Q16 delta_U = _Q16div(I_diff, delta_I_diff);
-        v_next -= delta_U;
+        _Q16 v_next = (v_min + v_max) / 2;
+        
+        if(Id(cell, v_next) > _Q16div(v_next, rl)){
+            v_min = v_next;
+        }else{
+            v_max = v_next;
+        }
     }
     
-    return v_next;
+    return (v_min + v_max) / 2;
 }
