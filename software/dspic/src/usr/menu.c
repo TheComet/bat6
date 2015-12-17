08813394e19bb071467152d2c504337d4e21cb37
Alex Murray <alex.murray@gmx.ch>
2015-12-17 20:23:14 +0100

    Twisting button selects items in the menu

--- software/dspic/src/usr/menu.c
+++ software/dspic/src/usr/menu.c
@@ -16,14 +16,20 @@ typedef enum menu_state_e
     STATE_NAVIGATE_PANELS
 } menu_state_e;
 
+struct selection_t
+{
+    short item;
+    short max;
+    short scroll;
+};
+
 struct menu_t
 {
     menu_state_e state;
-    short selected_item;
-    short max_items;
+    struct selection_t selection;
 };
 
-static struct menu_t menu = {};
+struct menu_t menu = {};
 
 static void on_button(unsigned int arg);
 
@@ -34,62 +40,106 @@ void menu_init(void)
 }
 
 /* -------------------------------------------------------------------------- */
-static void menu_update(void)
+static void handle_item_selection(unsigned int button)
 {
-    /*char buffer[40];*/
+    if(menu.selection.item == -1)
+        return;
 
-    switch(menu.state)
-    {
-        case STATE_NAVIGATE_MANUFACTURERS :
-            menu.max_items = solar_panels_get_manufacturers_count();
-            menu.selected_item = 0;
-            break;
-
-        case STATE_NAVIGATE_PANELS :
-            menu.max_items = solar_panels_get_panel_count(menu.selected_item);
-            menu.selected_item = 0;
-            break;
-    }
-}
-
-/* -------------------------------------------------------------------------- */
-static void handle_button_twist(unsigned int button)
-{
     if(button == BUTTON_TWISTED_LEFT)
     {
-        ++menu.selected_item;
-        menu.selected_item = (menu.selected_item == menu.max_items ?
-                0 : menu.selected_item);
+        --menu.selection.item;
+
+        if(menu.selection.item == -1)
+        {
+            menu.selection.item = menu.selection.max - 1;
+            menu.selection.scroll = menu.selection.max - 4;
+            if(menu.selection.scroll < 0)
+                menu.selection.scroll = 0;
+        }
+
+        if(menu.selection.item < menu.selection.scroll)
+            --menu.selection.scroll;
     }
 
     if(button == BUTTON_TWISTED_RIGHT)
     {
-        --menu.selected_item;
-        menu.selected_item = (menu.selected_item == -1 ?
-                menu.max_items -1 : menu.selected_item);
+        ++menu.selection.item;
+
+        if(menu.selection.item == menu.selection.max)
+        {
+            menu.selection.item = 0;
+            menu.selection.scroll = 0;
+        }
+
+        if(menu.selection.item - menu.selection.scroll >= 4)
+            ++menu.selection.scroll;
+    }
+}
+
+/* -------------------------------------------------------------------------- */
+static void handle_menu_switches(unsigned int button)
+{
+#define reset_selection()                                      \
+        menu.selection.item = (menu.selection.max ? 0 : -1);   \
+        menu.selection.scroll = 0;
+
+    switch(menu.state)
+    {
+        case STATE_NAVIGATE_MANUFACTURERS :
+
+            /* Switch to panel selection of the current manufacturer */
+            if(button == BUTTON_RELEASED && menu.selection.item != -1)
+            {
+                menu.selection.max =
+                        solar_panels_get_panel_count(menu.selection.item);
+                reset_selection();
+                menu.state = STATE_NAVIGATE_PANELS;
+            }
+
+            break;
+
+        case STATE_NAVIGATE_PANELS :
+
+            /* Switch back to manufacturer selection */
+            if(button == BUTTON_PRESSED_LONGER)
+            {
+                menu.selection.max = solar_panels_get_manufacturers_count();
+                reset_selection();
+                menu.state = STATE_NAVIGATE_MANUFACTURERS;
+            }
+            break;
+
+        default: break;
+    }
+}
+
+/* -------------------------------------------------------------------------- */
+static void menu_update(void)
+{
+    //char buffer[40];
+    int i;
+
+    for(i = menu.selection.scroll;
+        i < menu.selection.max && i != menu.selection.scroll + 4;
+        ++i)
+    {
+        switch(menu.state)
+        {
+            case STATE_NAVIGATE_MANUFACTURERS :
+                break;
+
+            case STATE_NAVIGATE_PANELS :
+                break;
+        }
     }
 }
 
 /* -------------------------------------------------------------------------- */
 static void on_button(unsigned int button)
 {
-    handle_button_twist(button);
+    handle_item_selection(button);
+    handle_menu_switches(button);
     menu_update();
-
-    switch(menu.state)
-    {
-        case STATE_NAVIGATE_MANUFACTURERS :
-            if(button == BUTTON_RELEASED)
-            {
-                
-            }
-            break;
-
-        case STATE_NAVIGATE_PANELS :
-            break;
-
-        default: break;
-    }
 }
 
 /* -------------------------------------------------------------------------- */
@@ -98,23 +148,115 @@ static void on_button(unsigned int button)
 /* Unit Tests */
 /* -------------------------------------------------------------------------- */
 
+#ifndef TESTING
+#define TESTING
+#error fuck you
+#endif
+
 #ifdef TESTING
 
 #include "gmock/gmock.h"
 
 using namespace ::testing;
 
-/* -------------------------------------------------------------------------- */
-class menu : public Test
+static void reset_menu_state()
 {
-    virtual void SetUp() {}
+    menu.state = STATE_NAVIGATE_MANUFACTURERS;
+    menu.selection.item = -1;
+    menu.selection.max = 0;
+    menu.selection.scroll = 0;
+}
+
+/* -------------------------------------------------------------------------- */
+class oled_menu : public Test
+{
+    virtual void SetUp()
+    {
+        reset_menu_state();
+    }
+
     virtual void TearDown() {}
 };
 
 /* -------------------------------------------------------------------------- */
-TEST_F(menu, example_test)
+TEST_F(oled_menu, twisting_right_with_no_items_does_nothing)
 {
-    EXPECT_THAT(1, Eq(1));
+    on_button(BUTTON_TWISTED_RIGHT);
+    EXPECT_THAT(menu.selection.item, Eq(-1));
+    EXPECT_THAT(menu.selection.max, Eq(0));
+    EXPECT_THAT(menu.selection.scroll, Eq(0));
+}
+
+TEST_F(oled_menu, twisting_left_with_no_items_does_nothing)
+{
+    on_button(BUTTON_TWISTED_LEFT);
+    EXPECT_THAT(menu.selection.item, Eq(-1));
+    EXPECT_THAT(menu.selection.max, Eq(0));
+    EXPECT_THAT(menu.selection.scroll, Eq(0));
+}
+
+TEST_F(oled_menu, item_selection_right_wraps_correctly)
+{
+    menu.selection.item = 0;
+    menu.selection.max = 5;
+    menu.selection.scroll = 0;
+
+    on_button(BUTTON_TWISTED_RIGHT);
+    EXPECT_THAT(menu.selection.item, Eq(1));
+    EXPECT_THAT(menu.selection.max, Eq(5));
+    EXPECT_THAT(menu.selection.scroll, Eq(0));
+
+    on_button(BUTTON_TWISTED_RIGHT);
+    EXPECT_THAT(menu.selection.item, Eq(2));
+    EXPECT_THAT(menu.selection.max, Eq(5));
+    EXPECT_THAT(menu.selection.scroll, Eq(0));
+
+    on_button(BUTTON_TWISTED_RIGHT);
+    EXPECT_THAT(menu.selection.item, Eq(3));
+    EXPECT_THAT(menu.selection.max, Eq(5));
+    EXPECT_THAT(menu.selection.scroll, Eq(0));
+
+    on_button(BUTTON_TWISTED_RIGHT);
+    EXPECT_THAT(menu.selection.item, Eq(4));
+    EXPECT_THAT(menu.selection.max, Eq(5));
+    EXPECT_THAT(menu.selection.scroll, Eq(1));
+
+    on_button(BUTTON_TWISTED_RIGHT);
+    EXPECT_THAT(menu.selection.item, Eq(0));
+    EXPECT_THAT(menu.selection.max, Eq(5));
+    EXPECT_THAT(menu.selection.scroll, Eq(0));
+}
+
+TEST_F(oled_menu, item_selection_left_wraps_correctly)
+{
+    menu.selection.item = 0;
+    menu.selection.max = 5;
+    menu.selection.scroll = 0;
+
+    on_button(BUTTON_TWISTED_LEFT);
+    EXPECT_THAT(menu.selection.item, Eq(4));
+    EXPECT_THAT(menu.selection.max, Eq(5));
+    EXPECT_THAT(menu.selection.scroll, Eq(1));
+
+    on_button(BUTTON_TWISTED_LEFT);
+    EXPECT_THAT(menu.selection.item, Eq(3));
+    EXPECT_THAT(menu.selection.max, Eq(5));
+    EXPECT_THAT(menu.selection.scroll, Eq(1));
+
+    on_button(BUTTON_TWISTED_LEFT);
+    EXPECT_THAT(menu.selection.item, Eq(2));
+    EXPECT_THAT(menu.selection.max, Eq(5));
+    EXPECT_THAT(menu.selection.scroll, Eq(1));
+
+    on_button(BUTTON_TWISTED_LEFT);
+    EXPECT_THAT(menu.selection.item, Eq(1));
+    EXPECT_THAT(menu.selection.max, Eq(5));
+    EXPECT_THAT(menu.selection.scroll, Eq(1));
+
+    on_button(BUTTON_TWISTED_LEFT);
+    EXPECT_THAT(menu.selection.item, Eq(0));
+    EXPECT_THAT(menu.selection.max, Eq(5));
+    EXPECT_THAT(menu.selection.scroll, Eq(0));
 }
 
 #endif /* TESTING */
